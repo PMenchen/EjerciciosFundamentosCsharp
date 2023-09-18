907ceb95f6ec77f02319d82cbe71dadbf7ca2872
PMenchen <72562238+PMenchen@users.noreply.github.com>
2023-09-18 18:53:47 +0200

    Añadir proyecto ya comenzado

---
+++ EjerciciosFundamentos/EjerciciosFundamentos/Program.cs
@@ -0,0 +1,46 @@
+﻿using EjerciciosFundamentos;
+
+
+//Ejercicio 1
+//Console.WriteLine("Ejercicio 1: \n\tEstado: {0}", Utilidades.ejercicio1(int.Parse(Console.ReadLine())) ? "Aprobado" : "Suspenso");
+
+//Ejercicio 2
+//Console.WriteLine("Ejercicio 2: \n\tTipo de número: {0}", Utilidades.ejercicio2(int.Parse(Console.ReadLine())) ? "Número par" : "Número impar");
+
+//Ejercicio 3.1
+/*Console.WriteLine("Inserte los valores de inicio y fin de la operación");
+int i = int.Parse(Console.ReadLine()), f = int.Parse(Console.ReadLine());
+var (suma, promedio) = Utilidades.ejercicio3A(i, f);
+Console.WriteLine("Ejercicio 3.1 \n\tLa suma del rango es: {0} \n\tEl promedio es: {1}", suma, promedio);
+
+//Ejercicio 3.2
+var (suma2, promedio2) = Utilidades.ejercicio3B(i, f);
+Console.WriteLine("Ejercicio 3.2 \n\tLa suma del rango es: {0} \n\tEl promedio es: {1}", suma2, promedio2);
+
+//Ejercicio 3.3
+var (suma3, promedio3) = Utilidades.ejercicio3C(i, f);
+Console.WriteLine("Ejercicio 3.3 \n\tLa suma del rango es: {0} \n\tEl promedio es: {1}", suma3, promedio3);
+
+//Ejercicio 3.4
+var (suma4, promedio4) = Utilidades.ejercicio3D(i, f);
+Console.WriteLine("Ejercicio 3.4 \n\tLa suma del rango es: {0} \n\tEl promedio es: {1}", suma4, promedio4);
+
+//Ejercicio 3.5
+var (suma5, promedio5) = Utilidades.ejercicio3E(i, f);
+Console.WriteLine("Ejercicio 3.5 \n\tLa suma del rango es: {0} \n\tEl promedio es: {1}", suma5, promedio5);
+
+//Ejercicio 3.6
+var (suma6, promedio6) = Utilidades.ejercicio3F(i, f);
+Console.WriteLine("Ejercicio 3.6 \n\tLa suma del rango es: {0} \n\tEl promedio es: {1}", suma6, promedio6
+
+//Ejercicio 4
+int limit = 50000;
+double h1 = Utilidades.ejercicio4(limit);
+double h2 = Utilidades.ejercicio4R(limit);
+Console.WriteLine("Ejercicio 4 \n\tEl resultado de la suma es: {0} \n\tEl resultado de la suma a la inversa es: {1}", h1, h2);
+//Hay una diferencia de 1.5*10^-14 entre las 2 operaciones);*/
+
+//Ejercicio 5
+int rep = 1000000;
+Console.WriteLine("Ejercicio 5\n\tEl resultado de pi con {0} términos utilizados es: {1}", rep, Utilidades.ejercicio5(rep));
+
---
+++ EjerciciosFundamentos/EjerciciosFundamentos/Utilidades.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EjerciciosFundamentos
+{
+    internal class Utilidades
+    {
+        //Ejercicio 1
+        public static bool ejercicio1(int mark)
+        {
+            if (!(mark >= 50)) return false;
+            return true;
+        }
+
+        //Ejercicio 2
+        public static bool ejercicio2(int number)
+        {
+            if (number % 2 == 0) return true;
+            return false;
+        }
+
+        //Ejercicio 3
+        public static (int, double) ejercicio3A(int start, int limit)
+        {
+
+            int sum = 0;
+            for (int i = start; i <= limit; i++)
+            {
+                sum += i;
+            }
+            double prom = (double) sum / limit;
+
+            return (sum, prom);
+        }
+
+        //Ejercicio 3.2
+        public static (int, double) ejercicio3B(int start, int limit)
+        {
+            int sum = 0;
+
+            while (start <= limit)
+            {
+                sum += start;
+                start++;
+            }
+
+            double prom = (double)sum / limit;
+
+            return (sum, prom);
+        }
+
+        //Ejercicio 3.3
+        public static (int, double) ejercicio3C(int start, int limit)
+        {
+            int sum = 0;
+
+            do { 
+                sum += start;
+                start++;
+            } while (start <= limit);
+            
+            double prom = (double)sum / limit;
+
+            return (sum, prom);
+        }
+
+        //Ejercicio 3.4
+        public static (int, double) ejercicio3D(int start, int limit)
+        {
+            int sum = 0, count = 0;
+
+            for (int i = start; i <= limit; i++)
+            {
+                if (!(i % 2 == 0))
+                {
+                    sum += i;
+                    count++;
+                }
+            }
+
+            double prom = (double)sum / count;
+
+            return (sum, prom);
+        }
+
+        //Ejercicio 3.5
+        public static (int, double) ejercicio3E(int start, int limit)
+        {
+            int sum = 0, count = 0;
+
+            for (int i = start; i <= limit; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    sum += i;
+                    count++;
+                }
+            }
+
+            double prom = (double)sum / count;
+
+            return (sum, prom);
+        }
+
+        //Ejercicio 3.6
+        public static (double, double) ejercicio3F(int start, int limit)
+        {
+            double sum = 0;
+            for (int i = start; i <= limit; i++)
+            {
+                sum += Math.Pow(i,2);
+            }
+            double prom = (double)sum / limit;
+
+            return (sum, prom);
+        }
+
+        //Ejercicio 4
+        public static double ejercicio4(int limit)
+        {
+            double h1 = 0;
+
+            for (int i = 1; i <= limit; i++)
+            {
+                h1 += (double) 1 / i;
+            }
+
+
+            return h1;
+        }
+        public static double ejercicio4R(int limit)
+        {
+            double h2 = 0;
+
+            for (int i = limit; i >= 1; i--)
+            {
+                h2 += (double) 1 / i;
+            }
+
+            return h2;
+        }
+
+        //Ejercicio 5
+        public static double ejercicio5(int rep)
+        {
+            /*int[] div = new int[loop];
+            int i = 0, n=1;
+            do
+            {
+                //div[i] = n;
+                i++;
+                n++;
+            } while (i<loop);*/
+
+            double pi = 0, aux = 0;
+            int n = 1;
+
+            for (int i = 0; i < rep; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    aux += (double) 1/n;
+                }
+                else
+                {
+                    aux -= (double) 1/n;
+                }
+                n = n + 2;
+            }
+
+            pi = 4 * aux;
+
+            return pi;
+        }
+    }
+}
